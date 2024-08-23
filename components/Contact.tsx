4a9bbd54be9788c60809a677300aea7ea6f61166
shubhu121 <shubhucsrldelhi@gmail.com>
2024-08-23 17:38:45 +0530

    mailto update

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -50,7 +50,7 @@ export default function Contact() {
 
       <p className="text-gray-700 -mt-6 dark:text-white/80">
         Please contact me directly at{" "}
-        <a className="underline" href="mailto:example@gmail.com">
+        <a className="underline" href="mailto:shubhendukumar125@gmail.com">
           shubhendukumar125@gmail.com
         </a>{" "}
         or through this form.
